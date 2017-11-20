0b114dd02a68e233d0b123db6128aacc7408b3f9
heanuea <g00318763@gmit.ie>
2017-11-20 19:49:36 +0000

    added new file and changed approach

--- predict.py
+++ predict.py
@@ -1,9 +1,32 @@
+#got the train and split from this stack over flow 
+#https://stackoverflow.com/questions/41859605/tensorflow-split-inputs-into-training-and-test-sets
+
 import numpy as np
-import pandas as pd
-from sklearn.neighbors import KNeighborsClassifier
+
+from sklearn import datasets
 from sklearn.model_selection import train_test_split
 
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
+from keras.utils import np_utils
 
-# I have a small dataset of features of 3 types of flowers. [Sepal length,Sepal width,Petal length,Petal width,Species]
-df = pd.read_csv('Iris.csv')
-df.head(10)
+iris = datasets.load_iris()
+
+
+# Split Train/Test
+x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.33)
+
+# Build a model
+model = Sequential()
+model.add(Dense(64, activation='relu', input_dim=x_train.shape[1]))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(set(iris.target)), activation='softmax'))
+
+model.compile(optimizer='rmsprop',
+             loss='categorical_crossentropy',
+             metrics=['accuracy'])
+
+
