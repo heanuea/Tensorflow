b45b481c0622893e1e549dc3e0d82b80cde67595
heanuea <g00318763@gmit.ie>
2017-11-21 12:06:27 +0000

    added model and output

--- predict.py
+++ predict.py
@@ -29,4 +29,23 @@ model.compile(optimizer='rmsprop',
              loss='categorical_crossentropy',
              metrics=['accuracy'])
 
+#epochs means how many times you go through your training set.
+# Set parameters
+epoch = 100
+batch_size = 10
 
+
+one_hot_label_y_train = np_utils.to_categorical(y_train)
+one_hot_label_y_test = np_utils.to_categorical(y_test)
+model.fit(x_train, one_hot_label_y_train, epochs=epoch, batch_size=batch_size)
+score = model.evaluate(x_test, one_hot_label_y_test, batch_size=batch_size)
+print("\n{}: {:.2f}%".format(model.metrics_names[1], score[1]*100))
+
+predict_data = np.array([4., 3., 4., 1.2])
+x = predict_data.reshape(-1,4)
+predict = model.predict(x)
+
+for i in range(len(predict)):    
+    guess = iris.target_names[np.argmax(predict[i])]
+    actual = iris.target_names[y_train[i]]
+    print("Predict: {},\nActual: {},\nIs it Correct: {}\n".format(guess, actual, guess==actual))
